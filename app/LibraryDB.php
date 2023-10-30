24d4064d057965f9d74a6065d6b21b6ad56713c6
Vladimir Kidyaev <krios2146@gmail.com>
2023-10-30 19:15:35 +0700

    feat: add init tables scripts to the LibararyDB.php

--- app/LibraryDB.php
+++ app/LibraryDB.php
@@ -19,13 +19,35 @@ class LibraryDB {
         );
 
         $this->initializeDatabase();
+        $this->initializeTables();
     }
 
     private function initializeDatabase() {
         // MySQL specific syntax, will not work with other DBMS
-        $this->pdo->exec("CREATE DATABASE IF NOT EXIST`" . self::DB_NAME . "`");
+        $this->pdo->exec("CREATE DATABASE IF NOT EXISTS`" . self::DB_NAME . "`");
 
         $this->pdo->exec("USE `" . self::DB_NAME . "`");
+    }
+
+    private function initializeTables() {
+        $this->pdo->exec("CREATE TABLE IF NOT EXISTS Users (
+            id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            first_name VARCHAR(50) NOT NULL,
+            last_name VARCHAR(50) NOT NULL,
+            email VARCHAR(30) NOT NULL UNIQUE
+        );");
+
+        $this->pdo->exec("CREATE TABLE IF NOT EXISTS Books (
+            id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            title VARCHAR(100) NOT NULL,
+            author VARCHAR(100) NOT NULL,
+            ISBN VARCHAR(30) NOT NULL UNIQUE,
+            genre VARCHAR(20),
+            publication_year INT NOT NULL,
+            is_available BOOLEAN NOT NULL,
+            user_id INT,
+            FOREIGN KEY (user_id) REFERENCES Users(id)
+        );");
     }
 
     public function get_all_books() {
